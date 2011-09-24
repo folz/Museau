870cfc8447999cc76609d2e91007a177b6bbb9fe
Rodney <folz@rodneyfolz.com>
2011-09-24 11:25:21 -0400

    Changes

--- webcli/static/init.js
+++ webcli/static/init.js
@@ -12,7 +12,9 @@
 					}).jPlayer("play", viewModel.seekTime());
 				},
 				ended: function (event) {
-					
+					$(this).jPlayer("setMedia", {
+						"m4a": viewModel.songUrl(),
+					}).jPlayer("play", viewModel.seekTime());
 				},
 				swfPath: "http://www.jplayer.org/2.1.0/js/Jplayer.swf",
 				supplied: "m4a",
@@ -20,28 +22,22 @@
 				preload: 'auto',
 				wmode: 'window',
 			});
-			
-			$("#jplayer_inspector").jPlayerInspector({jPlayer:$("#jquery_jplayer_1")});
 		}
 	};
 	
 	function ViewModel()
 	{
 		this.songUrl = ko.observable();
+		this.upcomingUrl = ko.observable();
 		this.seekTime = ko.observable();
 	}
 	window.viewModel = new ViewModel();
 	
 	$(document).ready(function(){
-		$.ajax({
-			type: "GET", datatype: "json",
-			url: '/ajax/all/',
-			success: function(data)
-			{
-				data = $.parseJSON(data);
-				viewModel.songUrl(data['songUrl']);
-				viewModel.seekTime(data['seekTime']);
-			}
+		$.getJSON('/ajax/all/', function(data)
+		{
+			viewModel.songUrl(data['songUrl']);
+			viewModel.seekTime(data['seekTime']);
 		});
 		
 		ko.applyBindings(viewModel);
