3835033896eb9647fd78ec27f69a83e1ed745ba1
Rodney <folz@rodneyfolz.com>
2011-11-12 21:09:29 -0800

    Inspirational quotes!

--- HackU11/usermanager/views.py
+++ HackU11/usermanager/views.py
@@ -7,6 +7,8 @@ from django.template import RequestContext
 from usermanager.models import *
 from usermanager.forms import *
 
+import random
+
 def index(req, login_form=LoginForm()):
 	if req.user.is_authenticated():
 		return render_to_response('home.html', {
@@ -16,6 +18,7 @@ def index(req, login_form=LoginForm()):
 		return render_to_response('index.html', {
 			'title': "Welcome to {0}".format(SITE_NAME),
 			'login_form': login_form,
+			'quote': quote()
 		}, context_instance=RequestContext(req))
 
 def login(req):
@@ -34,7 +37,7 @@ def login(req):
 				messages.add_message(req, messages.SUCCESS, "Welcome to {0}!".format(SITE_NAME))
 		else:
 			''' We had a failure at some point. Don't tell the user what it was, for Security Reasons. '''
-			messages.add_message(req, message.ERROR, "The username and password combination you entered was incorrect.")
+			messages.add_message(req, messages.ERROR, "The username and password combination you entered was incorrect.")
 	
 	return redirect('index')
 
@@ -43,3 +46,12 @@ def logout(req):
 	logout_user(req)
 	
 	return redirect('index')
+
+def quote():
+	return random.choice([
+		"""Surpass the impossible and kick reason to the curb! That's the Team Gurren way!""",
+		"""Whether impossible or laughable, we continue to walk the path of men!""",
+		"""Who the hell do you think we are?""",
+		"""A true man doesn't die even if he's killed!""",
+		"""We evolve beyond the person we were a minute before! Little by little, we advance a bit further with each turn! That's how a drill works!""",
+	])
